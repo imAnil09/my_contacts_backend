0767a0406dbe13061c915a6f0df43b326627dc02
Anil Bosi <91517934+imAnil09@users.noreply.github.com>
2024-01-23 00:32:33 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,12 @@ const {
   registerUser,
   loginUser,
   currentUser,
+  message
 } = require("../contollers/userController");
+router.get('/', message);
 const validateAccessToken = require("../middleWare/validateAccessToken");
 
 const router = express.Router();
-
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
