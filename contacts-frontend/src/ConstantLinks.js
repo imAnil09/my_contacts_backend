8fa6e69cb8c6f72bbdf20fcfc21e3ad76f175d7d
Anil Bosi <bosianils143@gmail.com>
2024-01-27 03:39:04 +0530

    changed the local host link with dev backend link

--- contacts-frontend/src/ConstantLinks.js
+++ contacts-frontend/src/ConstantLinks.js
@@ -1,5 +1,5 @@
-export const BASE_URL_CONTACTS = 'http://localhost:5001/api/contacts'
-export const BASE_URL_USERS = 'http://localhost:5001/api/users'
+export const BASE_URL_CONTACTS = 'https://my-contacts-backend-api-new.vercel.app/api/contacts'
+export const BASE_URL_USERS = 'https://my-contacts-backend-api-new.vercel.app/api/users'
 export const LOGIN = '/login';
 export const SIGNUP = '/signup';
 export const HOME = '/';
