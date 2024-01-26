ca531e325172f5ede79561a5cf8ca6f84267ae30
Anil Bosi <bosianils143@gmail.com>
2024-01-27 03:09:33 +0530

    changed the createReducer code with builder structure

--- contacts-frontend/src/Store/Reducers/AuthReducer.js
+++ contacts-frontend/src/Store/Reducers/AuthReducer.js
@@ -1,8 +1,16 @@
+// AuthReducer.js
+
 import { createReducer } from "@reduxjs/toolkit";
 
-const initialState = ''
-export const AuthReducer = createReducer(initialState, {
-    accessToken: (state, action) => {
-        return action.payload;
-    }
-})
+const initialState = '';
+
+export const AuthReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase('accessToken', (state, action) => {
+      return action.payload;
+    })
+    // Add other cases if needed
+    .addDefaultCase((state) => {
+      return state;
+    });
+});
--- contacts-frontend/src/Store/Reducers/ContactsList.js
+++ contacts-frontend/src/Store/Reducers/ContactsList.js
@@ -3,8 +3,13 @@ import { createReducer } from "@reduxjs/toolkit";
 
 const initialState = [];
 
-export const ContactsList = createReducer(initialState, {
-  contactsList: (state, action) => {
-    return action.payload;
-  },
+export const ContactsList = createReducer(initialState, (builder) => {
+  builder
+    .addCase('contactsList', (state, action) => {
+      return action.payload;
+    })
+    // Add other cases if needed
+    .addDefaultCase((state) => {
+      return state;
+    });
 });
